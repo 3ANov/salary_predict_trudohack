ab57166422303889b46a3f06bfda1faa2d7f7594
3ANov <3anovdev@gmail.com>
2020-07-06 16:10:48 +0300

    Пилю пользовательский интерфейс

--- server.py
+++ server.py
@@ -1,13 +1,22 @@
 from flask import Flask, render_template
+from flask_restful import reqparse, abort, Api, Resource
 
 app = Flask(__name__)
 app.config.from_object('config')
+api = Api(app)
 
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
+@app.route('/', methods=['GET'])
+def main():
     return render_template('index.html')
 
 
-if __name__ == "__main__":
+class NeuralResult(Resource):
+    def post(self):
+        return {1: 22}
+
+
+api.add_resource(NeuralResult, '/')
+
+if __name__ == '__main__':
     app.run()
