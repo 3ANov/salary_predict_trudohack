6157ad491187387b24afff55420567208f6034d9
3ANov <3anovdev@gmail.com>
2020-07-10 22:46:51 +0300

    изменил путь до базы (для heroku)

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
+import os
+
+
 class Config(object):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'postgresql://testuser:testuser@localhost/trudohack_ml_db'
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     SQLALCHEMY_TRACK_MODIFICATIONS = False
