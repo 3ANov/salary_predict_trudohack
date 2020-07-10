a2324d477c74a1005d460e75ae32e595dce80bda
3ANov <3anovdev@gmail.com>
2020-07-10 15:58:30 +0300

    нужен рефакторинг - но работает предсказание на тестовом сервере

--- server.py
+++ server.py
@@ -16,8 +16,28 @@ api = Api(app)
 parser = reqparse.RequestParser()
 parser.add_argument('input_vacancy_text', type=str, help='Vacancy text')
 
-from neural_predict import print_prediction
+#from neural_predict import print_prediction
 
+import re
+import sklearn
+import pickle
+
+class NeuralModel(db.Model):
+    file_name = db.Column(db.String, primary_key=True)
+    file = db.Column(db.LargeBinary)
+
+
+TOKEN_RE = re.compile(r'[\w\d]+')
+
+
+def tokenize_text_simple_regex(txt, min_token_size=4):
+    """ This func tokenize text with TOKEN_RE applied ealier """
+    txt = txt.lower()
+    all_tokens = TOKEN_RE.findall(txt)
+    return [token for token in all_tokens if len(token) >= min_token_size]
+
+
+loaded_model = pickle.loads(NeuralModel.query.filter_by(file_name='model_one').first().file)
 
 @app.route('/', methods=['GET'])
 def main():
@@ -30,7 +50,7 @@ class NeuralResult(Resource):
         print(args)
         #принимаем строку - возвращаем её длину
         #как победить кодировку?
-        return {args['input_vacancy_text']: print_prediction(args['input_vacancy_text'])}, 200
+        return {args['input_vacancy_text']: loaded_model.predict([args['input_vacancy_text']]).tolist()[0]}, 200
 
 
 api.add_resource(NeuralResult, '/')
